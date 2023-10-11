62a9230e329e657c6260a966441b0f45a9aef870
Chofo's Machine <diegovalenciavives@gmail.com>
2023-10-11 19:05:08 -0300

    Ajustada duracion de tokens

--- API/settings.py
+++ API/settings.py
@@ -136,6 +136,6 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=9),
+    "REFRESH_TOKEN_LIFETIME": timedelta(weeks=1),
 }
