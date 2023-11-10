2c65d1dd94b307368cb701d2a81cb075a1fe706a
GoroGabox <alfons.diaz97@gmail.com>
2023-11-10 18:10:27 -0300

    Nuevas views para controlar productos dentro de un pedido, correccion comportamiento para crear un nuevo pedido

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,5 +1,6 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from restaurant.views import IngredienteViewSet, IngredienteSimpleViewSet, CategoriaViewSet, ProductoViewSet, ProductoIngredienteViewSet, PedidoViewSet, PedidoProductoViewSet, EstadoPedidoViewSet, ProductoSimpleViewSet
+from restaurant.views import IngredienteViewSet, IngredienteSimpleViewSet, CategoriaViewSet, ProductoViewSet, ProductoIngredienteViewSet, PedidoViewSet, PedidoProductoViewSet, EstadoPedidoViewSet, ProductoSimpleViewSet, PedidoProductoUpdateView, PedidoProductoDeleteView
 
 router = DefaultRouter()
 router.register(r'ingredientes', IngredienteViewSet)
@@ -12,4 +13,9 @@ router.register(r'pedidos', PedidoViewSet)
 router.register(r'pedidos-productos', PedidoProductoViewSet)
 router.register(r'estado-pedido', EstadoPedidoViewSet)
 
-urlpatterns = router.urls
+generics_urls = [
+    path('pedidos/<int:pedido_id>/productos/<int:producto_id>/', PedidoProductoUpdateView.as_view(), name='pedido-producto-update'),
+    path('pedidos/<int:pedido_id>/productos/<int:producto_id>/', PedidoProductoDeleteView.as_view(), name='pedido-producto-delete'),
+]
+
+urlpatterns = generics_urls + router.urls
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.db import transaction
+from rest_framework import status
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from .models import Ingrediente, Categoria, Producto, ProductoIngrediente, Pedido, PedidoProducto, EstadoPedido
-from .serializers import IngredienteSerializer, IngredienteSimpleSerializer, CategoriaSerializer, ProductoSerializer, ProductoIngredienteSerializer, ProductoSimpleSerializer, PedidoSerializer, PedidoProductoSerializer, EstadoPedidoSerializer
+from .serializers import IngredienteSerializer, IngredienteSimpleSerializer, CategoriaSerializer, ProductoSerializer, ProductoIngredienteSerializer, ProductoSimpleSerializer, PedidoSerializer, PedidoProductoSerializer, EstadoPedidoSerializer, PedidoSimpleSerializer
 from rest_framework.permissions import IsAdminUser
 from .permissions import RoleBasedPermission
 
@@ -59,6 +62,37 @@ class PedidoViewSet(ModelViewSet):
     queryset = Pedido.objects.all()
     serializer_class = PedidoSerializer
     # permission_classes = [RoleBasedPermission]
+
+    @action(detail=False, methods=['post'])
+    def create_pedido_with_productos(self, request):
+        pedido_data = request.data
+        productos_ids = pedido_data.pop('productos', [])
+        serializer = self.get_serializer(data=pedido_data)
+
+        if serializer.is_valid(raise_exception=True):
+            pedido = serializer.save()
+
+            productos = Producto.objects.filter(id__in=productos_ids)
+            pedido.productos.set(productos)
+
+            return Response(PedidoSerializer(pedido).data, status=status.HTTP_201_CREATED)
+        
+    #Espera recibir los productos como una lista de IDs    
+    # {
+    #     ...
+    #     "nombre_cliente": "Nombre del Cliente",
+    #     "mesa": 5,
+    #     "productos": [1, 2, 3]  // Lista de IDs de productos
+    # }
+
+class PedidoProductoUpdateView(generics.UpdateAPIView):
+    queryset = PedidoProducto.objects.all()
+    serializer_class = PedidoProductoSerializer
+
+class PedidoProductoDeleteView(generics.DestroyAPIView):
+    queryset = PedidoProducto.objects.all()
+    serializer_class = PedidoProductoSerializer
+
 
 class PedidoProductoViewSet(ModelViewSet):
     queryset = PedidoProducto.objects.all()
